37ab0ee6b9a4ecc116e443e86cbe3721c7469280
Tino <tinomuzambi@gmail.com>
2021-02-10 23:34:55 +0200

    Adds title to status div.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const STATUSES = ["green", "yellow", "red"];
+const TITLES = ["Got it", "Working on it", "Still gotta get to it"];
 
 const statusEls = document.querySelectorAll(".status");
 
-statusEls.forEach((statusEl) => {
+statusEls.forEach((statusEl, i) => {
 	statusEl.addEventListener("click", () => {
 		const currStatus = statusEl.classList.value.split(" ")[1];
 		statusEl.classList = `status ${getNext(currStatus)}`;
@@ -29,6 +30,7 @@ const getFromLS = () => {
 	let saveStatuses = JSON.parse(localStorage.getItem("cubing-statuses"));
 	statusEls.forEach((statusEl, i) => {
 		statusEl.classList = `status ${saveStatuses[i]}`;
+		statusEl.title = TITLES[i];
 	});
 };
 
