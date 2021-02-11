1042d7c3e3bc72bcc7269e082e1e19fb161cb8b0
Tino <tinomuzambi@gmail.com>
2021-02-11 21:20:56 +0200

    Moves firebase config into script.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const STATUSES = ["red", "yellow", "green"];
 const TITLES = ["Still gotta get to it", "Working on it", "Got it"];
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBZzYFYsjJ6B5KJDvgtIY2pGswOkl_q0qM",
+	authDomain: "cubingalgos.firebaseapp.com",
+	projectId: "cubingalgos",
+	storageBucket: "cubingalgos.appspot.com",
+	messagingSenderId: "38468774650",
+	appId: "1:38468774650:web:4aa0b84fcea425df0b3743",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 const statusEls = document.querySelectorAll(".status");
 
 statusEls.forEach((statusEl, i) => {
@@ -34,12 +47,17 @@ const saveToLS = () => {
 };
 
 // Get current statuses from local storage.
-const getFromLS = () => {
+const getFromLS = async () => {
 	let saveStatuses = JSON.parse(localStorage.getItem("cubing-statuses"));
 	// Set classlist based on status.
 	statusEls.forEach((statusEl, i) => {
 		statusEl.classList = `status ${saveStatuses[i]}`;
 		statusEl.title = TITLES[STATUSES.indexOf(saveStatuses[i])];
+	});
+	let dbStatuses;
+	const snapshot = await db.collection("statuses").get();
+	snapshot.forEach((doc) => {
+		console.log(doc.data().statuses);
 	});
 };
 
