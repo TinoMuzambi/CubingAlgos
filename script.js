85e5553affe9621480902b960ef6bd83545791ae
Tino <tinomuzambi@gmail.com>
2021-02-11 21:54:34 +0200

    Adds saving to db.

--- script.js
+++ script.js
@@ -41,30 +41,37 @@ const getNext = (status) => {
 };
 
 // Save current statuses to local storage.
-const saveToLS = () => {
+const saveToLS = async () => {
 	let saveStatuses = [];
 	// Get status based on classlist.
 	statusEls.forEach((statusEl) => {
 		saveStatuses.push(statusEl.classList.value.split(" ")[1]);
 	});
 	localStorage.setItem("cubing-statuses", JSON.stringify(saveStatuses));
+	const statusesRef = db.collection("statuses").doc("q6UaTNEWbgTTFfcQUXiN");
+
+	await statusesRef.set({
+		statuses: saveStatuses,
+	});
+
 	// Call get to update UI.
 	getFromLS();
 };
 
 // Get current statuses from local storage.
 const getFromLS = async () => {
-	// let saveStatuses = JSON.parse(localStorage.getItem("cubing-statuses"));
-	// Set classlist based on status.
 	let dbStatuses;
+	// Get statuses from db.
 	const snapshot = await db.collection("statuses").get();
 	snapshot.forEach((doc) => {
 		dbStatuses = doc.data().statuses;
 	});
+	// Set classlist based on status.
 	statusEls.forEach((statusEl, i) => {
 		statusEl.classList = `status ${dbStatuses[i]}`;
 		statusEl.title = TITLES[STATUSES.indexOf(dbStatuses[i])];
 	});
+	// Get rid of spinner once data is loaded.
 	spinner.classList.add("finish");
 };
 
